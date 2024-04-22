1eda34dd930e6c9f34d2dd77228ef8ad9cf9db0e
Rasel Ahmed <raselbabu34.bd@gmail.com>
2024-04-22 18:54:53 +0600

    exel import

--- app/Livewire/GenerateFromFile.php
+++ app/Livewire/GenerateFromFile.php
@@ -2,18 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Imports\NameImport;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Maatwebsite\Excel\Facades\Excel;
+use Spatie\SimpleExcel\SimpleExcelReader;
 
 class GenerateFromFile extends Component
 {
+	use WithFileUploads;
 	public array $is_customs = [
 		'position_x'=>false,
 		'position_y'=>false,
 		'anchor_X' => false,
 		'anchor_y' => false
 	];
+	
+	public $file;
+	
     public function render()
     {
         return view('livewire.generate-from-file');
     }
+	
+	
+	public function generate()
+	{
+//		dd($this->file);
+		$data  = Excel::toArray(new NameImport(),$this->file);
+		dd($data);
+	}
 }
--- resources/views/livewire/generate-from-file.blade.php
+++ resources/views/livewire/generate-from-file.blade.php
@@ -2,10 +2,10 @@
     @php
         use \DantSu\PHPImageEditor\Image;
     @endphp;
-    <form class="max-w-md mx-auto">
+    <form class="max-w-md mx-auto" wire:submit="generate">
         <div class="mb-5">
             <label class="label" for="file_input">Upload file</label>
-            <input class="file-input" id="file_input" type="file">
+            <input class="file-input" wire:model="file" id="file_input" type="file">
         </div>
         <div class="flex gap-x-5 mb-5 w-full">
             <div class="flex-1">
@@ -95,41 +95,18 @@
         </div>
         <div class="flex gap-x-5 mb-5 w-full">
             <div class="flex-1">
-                <label for="anchor_X" class="label !flex items-center gap-x-2">
-                    Anchor X
-                    <label class="inline-flex items-center cursor-pointer">
-                        <input type="checkbox" wire:model.live="is_customs.anchor_X" class="sr-only peer">
-                        <div class="relative w-7 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all  peer-checked:bg-blue-600"></div>
-                    </label>
+                <label for="rotation" class="label">
+                    Rotation
                 </label>
-                @if($is_customs['anchor_X'])
-                    <input type="number" id="anchor_X" wire:model="anchor_X" class="number" placeholder="00.00" />
-                @else
-                    <select id="anchor_X" class="select" wire:model="anchor_X">
-                        <option selected value="{{Image::ALIGN_LEFT}}">Left</option>
-                        <option value="{{Image::ALIGN_CENTER}}">Center</option>
-                        <option value="{{Image::ALIGN_RIGHT}}">Right</option>
-                    </select>
-                @endif
+                <input type="number" id="rotation" wire:model="rotation" class="number" placeholder="00.00" />
             </div>
             <div class="flex-1">
-                <label for="anchor_y" class="label !flex items-center gap-x-2">
-                    Anchor Y
-                    <label class="inline-flex items-center cursor-pointer">
-                        <input type="checkbox" wire:model.live="is_customs.anchor_y" class="sr-only peer">
-                        <div class="relative w-7 h-4 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all  peer-checked:bg-blue-600"></div>
-                    </label>
+                <label for="letterSpacing" class="label">
+                    Letter Spacing
                 </label>
-                @if($is_customs['anchor_y'])
-                    <input type="number" id="anchor_y" wire:model="anchor_y" class="number" placeholder="00.00" />
-                @else
-                    <select id="anchor_y" class="select" wire:model="anchor_y">
-                        <option selected value="{{Image::ALIGN_TOP}}">Top</option>
-                        <option value="{{Image::ALIGN_MIDDLE}}">Middle</option>
-                        <option value="{{Image::ALIGN_BOTTOM}}">Bottom</option>
-                    </select>
-                @endif
+                <input type="number" id="letterSpacing" wire:model="letterSpacing" class="number" placeholder="00.00" />
             </div>
         </div>
+        <button type="submit" class="bg-green-600 text-white py-2 px-5 rounded">Generate</button>
     </form>
 </div>
