bc4e634b080185dde2919e03e8d0bec4d7067739
Rasel Ahmed <raselbabu34.bd@gmail.com>
2024-04-24 16:54:05 +0600

    add seed user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::create([
+			'name' => 'Rasel Babu',
+			'email' => 'rasel@gmail.com',
+			'email_verified_at' => now(),
+			'password' => Hash::make('password'),
+			'remember_token' => Str::random(10),
+		]);
     }
 }
